265c93db098c50e6a85e64b8922d4462686900ed
izrec <stratterabyer@gmail.com>
2023-07-12 12:37:23 +0300

    тест коммит

---
+++ TOP_automation/DZ15/Find_Element_Chrome.py
@@ -0,0 +1,35 @@
+# Открыть браузер Chrome  и загрузить страницу http://example.com
+# Найти элемент на странице с помощью локатора  и проверить, что его
+# текст соответствует ожидаемому значению.Выполнить клик по кнопке
+# на странице и убедиться\  , что страница была успешно перезагружена.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import  WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+browser = webdriver.Chrome()
+link = ('http://example.com')
+browser.get(link)
+locator = (By.XPATH, "/html/body/div/p[2]/a")
+try:
+    find = WebDriverWait(browser, 10).until(EC.presence_of_element_located(locator))
+except:
+    browser.quit()
+    print('No such element')
+
+if find.text == 'More information...':
+    url1 = browser.current_url
+    find.click()
+    time.sleep(4)
+    url2 = browser.current_url
+    print('The element found and fits the text')
+else:
+    print('The text if not found')
+
+
+if url1 != url2:
+    print('The url has been changed')
+else:
+    print('The url has not been changed')
